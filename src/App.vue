157d4c536d661681a7b83de446cedb445dc79c80
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-20 11:21:19 +0100

    sfunkcnenie pridavania a odstranovania uloh

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <input v-model="input">
+  <input v-model="input" @keyup.enter="addItem()">
   <button @click="addItem()">Add to list</button>
 
   <h2>Polozky</h2>
   <ul>
     <li v-for="item in validItems" :key="`item-${item.id}`">
       <span @click="deleteItem(item)" style="margin-right: 15px">X</span>
-      <span>Nazov polozky</span>
       {{ item.text }}
     </li>
   </ul>
@@ -16,15 +15,18 @@
   export default {
     data() {
       return {
-        input: "",
-        list: []
+        items: [],
+        input: ""
       }
     },
 
     methods: {
       addItem() {
-        this.list.push({
-          id: this.list.lenght+1,
+        
+        if (this.input.length===0) return;
+        
+        this.items.push({
+          id: this.items.lenght+1,
           text: this.input,
           is_deleted: false
         })
@@ -32,12 +34,16 @@
       },
 
       deleteItem(item) {
-        item.is_deleted=true
+        item.is_deleted = true
       }
     },
 
     computed: {
       validItems() {
+        return this.items.filter(item => !item.is_deleted)
+      },
+
+      deletedItems() {
         return this.items.filter(item => item.is_deleted)
       }
     }
