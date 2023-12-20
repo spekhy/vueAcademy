59167ea04a75d686c89731d4d3c3babf36ad08fd
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-20 10:27:26 +0100

    pridavanie a deletovanie, zatial nefunkcne

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,51 @@
 <template>
+  <input v-model="input">
+  <button @click="addItem()">Add to list</button>
 
-<input v-model = "input">
-<button @click = "addItem()">Add to list</button>
-
+  <h2>Polozky</h2>
+  <ul>
+    <li v-for="item in validItems" :key="`item-${item.id}`">
+      <span @click="deleteItem(item)" style="margin-right: 15px">X</span>
+      <span>Nazov polozky</span>
+      {{ item.text }}
+    </li>
+  </ul>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        input: "",
+        list: []
+      }
+    },
 
-export default {
-  data() {
-    return {
-      input: "",
-      list: []
-    }
-  },
-  methods: {
-    addItem() {
-      this.list.push({
-        id: this.list.lenght+1,
-        text: this.input,
-        is_deleted: false
-      })
-      this.input = ""
+    methods: {
+      addItem() {
+        this.list.push({
+          id: this.list.lenght+1,
+          text: this.input,
+          is_deleted: false
+        })
+        this.input = ""
+      },
+
+      deleteItem(item) {
+        item.is_deleted=true
+      }
+    },
+
+    computed: {
+      validItems() {
+        return this.items.filter(item => item.is_deleted)
+      }
     }
   }
-}
-
 </script>
 
 <style>
 
-body {
-  background-color: black;
-}
+
 
 </style>
