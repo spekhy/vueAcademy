820ecf04a9761646f77e15f22765bd66709bb4c4
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-20 15:57:44 +0100

    simple stylovanie

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
-<template>
-  <input v-model="input" @keyup.enter="addItem()">
-  <button @click="addItem()">Add to list</button>
+<template class="bg-black">
+  <input v-model="input" @keyup.enter="addItem()" class="border-[2px] border-black rounded-[5px] bg-stone-700 m-[10px]">
+  <button @click="addItem()" class="border-[1px] border-white rounded-[5px] mx-[5px] px-[5px]">Add to list</button>
 
-  <h2>Polozky</h2>
+  <hr class="my-[10px]">
+
+  <h2 class="ml-[10px]">Polozky</h2>
   <ul>
     <li v-for="item in validItems" :key="`item-${item.id}`">
-      <span @click="deleteItem(item)" class="mx-[15px]">X</span>
+      <span @click="deleteItem(item)" class="mx-[20px]">X</span>
       {{ item.text }}
     </li>
   </ul>
+
+  <hr  class="my-[10px]">
+
 </template>
 
 <script>
@@ -52,6 +57,9 @@
 
 <style>
 
-
+  body {
+    background-color: black;
+    color: white;
+  }
 
 </style>
