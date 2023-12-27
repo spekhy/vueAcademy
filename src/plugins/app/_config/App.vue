b0a472cbb1b559cad8552ffff4ae01871696a9a6
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-27 13:39:00 +0100

    trochu som rewritol router, riesim filepaths

--- src/plugins/app/_config/App.vue
+++ src/plugins/app/_config/App.vue
@@ -1,27 +1,28 @@
-<template class="bg-black">
+<template>
   
   <div>
-    <input v-model="input" @keyup.enter="addItem()" class="border-[2px] border-black rounded-[5px] m-[10px]">
-    <button @click="addItem()" class="border-[1px] border-white rounded-[5px] mx-[5px] px-[5px]">Add to list</button>
+    <router-view>
+      <input v-model="input" @keyup.enter="addItem()" class="border-[2px] border-black rounded-[5px] m-[10px]">
+      <button @click="addItem()" class="border-[1px] border-white rounded-[5px] mx-[5px] px-[5px]">Add to list</button>
 
-    <hr class="my-[10px]">
+      <hr class="my-[10px]">
 
-    <h1 class="ml-[10px] font-semibold">Polozky</h1>
-    <ul>
-      <li v-for="item in validItems" :key="`item-${item.id}`">
-        <span @click="deleteItem(item)" class="mx-[20px]">X</span>
-        {{ item.text }}
-      </li>
-    </ul>
+      <h1 class="ml-[10px] font-semibold">Polozky</h1>
+      <ul>
+        <li v-for="item in validItems" :key="`item-${item.id}`">
+          <span @click="deleteItem(item)" class="mx-[20px]">X</span>
+          {{ item.text }}
+        </li>
+      </ul>
 
-    <hr  class="my-[10px]">
+      <hr  class="my-[10px]">
 
-    <ul>
-      <li v-for="item in deletedItems" :key="`item-${item.id}`" class="mx-[20px]">
-        <s><span> - </span>{{ item.text }}</s>
-      </li>
-    </ul>
-    <router-view></router-view>
+      <ul>
+        <li v-for="item in deletedItems" :key="`item-${item.id}`" class="mx-[20px]">
+          <s><span> - </span>{{ item.text }}</s>
+        </li>
+      </ul>
+    </router-view>
   </div>
 
 </template>
@@ -68,7 +69,7 @@
 <style>
 
   body {
-    background-color: black;
+    background-color: rgb(0, 0, 0);
     color: white;
   }
   #app {
--- src/plugins/app/_config/router.ts
+++ src/plugins/app/_config/router.ts
@@ -1,20 +1,23 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import shoppingList from '/src/plugins/lib@shopping-list/shopping-lists/shopping-lists.vue'
+import shoppingDetail from '/src/plugins/lib@shopping-list/shopping-list-detail/shopping-list-detail.vue'
+import notFound from '/src/plugins/app/views/not-found.vue'
 
 const routes = [
     {
-        path: '/src/plugins/lib@shopping-list/shopping-lists/shopping-lists.vue',
+        path: '/shopping-list',
         name: 'Shopping List - List',
-        component: ''
+        component: shoppingList
     },
     {
-        path: '/src/plugins/lib@shopping-list/shopping-list-detail/shopping-list-detail.vue',
+        path: '/shopping-list-detail',
         name: 'Shopping List - Detail',
-        component: ''
+        component: shoppingDetail
     },
     {
-        path: '/src/plugins/app/views/not-found.vue',
+        path: '/not-found',
         name: 'NotFound',
-        component: ''
+        component: notFound
     }
 ]
 
