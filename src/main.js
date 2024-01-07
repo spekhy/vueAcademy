86ecffd17d4d3037356d75b9ca4bd183fa9a83e2
spekhy <145577204+spekhy@users.noreply.github.com>
2024-01-07 21:19:12 +0100

    vyriesenie problemu s routerom, premenovanie filu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './plugins/app/_config/App.vue'
-import router from './plugins/app/_config/router.ts'
+import router from './plugins/app/_config/router.js'
 import './assets/tailwind.css'
 
 createApp(App)
--- src/plugins/app/_config/App.vue
+++ src/plugins/app/_config/App.vue
@@ -2,83 +2,19 @@
   
   <div>
     <router-view>
-      <input v-model="input" @keyup.enter="addItem()" class="border-[2px] border-black rounded-[5px] m-[10px]">
-      <button @click="addItem()" class="border-[1px] border-white rounded-[5px] mx-[5px] px-[5px]">Add to list</button>
-
-      <hr class="my-[10px]">
-
-      <h1 class="ml-[10px] font-semibold">Polozky</h1>
-      <ul>
-        <li v-for="item in validItems" :key="`item-${item.id}`">
-          <span @click="deleteItem(item)" class="mx-[20px]">X</span>
-          {{ item.text }}
-        </li>
-      </ul>
-
-      <hr  class="my-[10px]">
-
-      <ul>
-        <li v-for="item in deletedItems" :key="`item-${item.id}`" class="mx-[20px]">
-          <s><span> - </span>{{ item.text }}</s>
-        </li>
-      </ul>
     </router-view>
   </div>
 
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [],
-        input: ""
-      }
-    },
+  
 
-    methods: {
-      addItem() {
-        
-        if (this.input.length===0) return;
-        
-        this.items.push({
-          id: this.items.lenght+1,
-          text: this.input,
-          is_deleted: false
-        })
-        this.input = ""
-      },
-
-      deleteItem(item) {
-        item.is_deleted = true
-      }
-    },
-
-    computed: {
-      validItems() {
-        return this.items.filter(item => !item.is_deleted)
-      },
-
-      deletedItems() {
-        return this.items.filter(item => item.is_deleted)
-      }
-    }
-  }
+  
 </script>
 
 <style>
 
-  body {
-    background-color: rgb(0, 0, 0);
-    color: white;
-  }
-  #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 
 </style>
